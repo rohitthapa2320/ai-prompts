f394d96ef4dddecb8d2e7170a520ee64402e1677
rohitthapa2320 <rohitthapa2362000@gmail.com>
2024-05-16 12:37:55 +0530

    Comment Sesnitive data

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,24 +1,28 @@
-import '@styles/globals.css';
+import "@styles/globals.css";
+
+import Navbar from "@components/Navbar";
+import Provider from "@components/Provider";
 
 export const metadata = {
   title: "PromptShare",
-  description: "Unreveal and Share AI Prompts"
-}
+  description: "Discover & Share AI Prompts",
+};
 
-const RootLayout = ({children}) => {
-  return (
-    <html lang='en'>
-      <body>
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+      <Provider>
         <div className='main'>
-          <div className='gradient'/>
+          <div className='gradient' />
         </div>
 
         <main className='app'>
+          <Navbar />
           {children}
         </main>
-      </body>
-    </html>
-  )
-}
+      </Provider>
+    </body>
+  </html>
+);
 
 export default RootLayout;
